10666a781f4b816ef5bc712cdfd404fa390324c6
Adriano Paulo <paulocadeth100@gmail.com>
2023-08-27 21:10:51 +0100

    Exercicios/ ex002

---
+++ cursopython/Exercicios/ex002.py
@@ -0,0 +1,7 @@
+nome = str(input("Digite o um nome: "))
+idade = int(input("Digite a sua idade: "))
+
+if idade >= 18:
+    print(f"{nome} é Maior de idade")
+else:
+    print(f"{nome} é Menor de idade")
