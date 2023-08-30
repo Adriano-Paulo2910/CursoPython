b7e4422af13a02c0c38a6ea8a3e76fd2a6bcffa0
Adriano Paulo <paulocadeth100@gmail.com>
2023-08-30 17:47:48 +0100

    Exercicios 3 e 4

---
+++ cursopython/aula12/aula12.py
@@ -0,0 +1,18 @@
+"""
+
+numero1 = 12
+numero2 = 13
+
+divisao = numero1 / numero2
+
+print("{:.2f}" .format(divisao))
+
+nome = 'Adriano'
+sobrenome = 'Paulo'
+
+nomeFormatado = '{1:#^20}'.format(nome, sobrenome)
+print(nomeFormatado)
+"""
+
+
+
